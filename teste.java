c7b754caae663a64b18257a4f51cf2bd750b5448
Diogo Luiz Zago <diogoluizago@gmail.com>
2025-05-20 15:37:38 -0300

    Delete teste.java

--- teste.java
+++
@@ -1,7 +0,0 @@
-public class teste {
-    public static void main(String[] args) {
-        int valor = Integer.parseInt("123");  // Lança NumberFormatException
-System.out.println(valor);
-    }
-}
-
