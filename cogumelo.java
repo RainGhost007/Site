98b72afda14720670da48ef63b6f09592c0fc3c8
Diogo Luiz Zago <diogoluizago@gmail.com>
2025-05-20 15:37:51 -0300

    Delete cogumelo.java

--- cogumelo.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class cogumelo {
-	public static void main(String [] args){
-		Scanner sc1 = new Scanner(System.in);
-		int num = sc1.nextInt();
-		System.out.println(num);
-	}
-}
