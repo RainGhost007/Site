b215c17d0f4e8ff51363cc0911598163e8ab663c
Diogo Luiz Zago <diogoluizago@gmail.com>
2025-06-04 10:15:23 -0300

    Cogumelos.java 1.1

--- Cogumelos.java
+++ Cogumelos.java
@@ -10,48 +10,47 @@ public class Cogumelos {
 	public static void main(String[] args) {
 	Scanner sc1 = new Scanner(System.in);
 		
-		int i = 0;
-		int[][] cogus = new int[800][];
+	int i = 0;
+	int[][] cogus = new int[800][]; // o input que estou usando tem 800 linhas, mas se  quiser diminuir é só alterar esse número;
+					// caso não saiba o números de linhas, faça um método que adiciona vetores a uma matriz;
+	while (true) {
 		
-		while (true) {
-		
-			String cogumelos = sc1.nextLine();
+		String cogumelos = sc1.nextLine();
 			
-			if (i >= cogus.length) break;
-			if (cogumelos.isEmpty()) break;
+		if (i >= cogus.length) break;
+		if (cogumelos.isEmpty()) break;
 			
-			String[] vetor = cogumelos.split(",");
+		String[] vetor = cogumelos.split(",");
 
-			int[] cogu = new int[vetor.length];
+		int[] cogu = new int[vetor.length];
     		for (int j = 0; j < vetor.length; j++) {
         		cogu[j] = Integer.parseInt(vetor[j]);
     		}
 			cogus[i] = cogu;
 			i++;
 			
-		}
+	}
 		int mod = 1000000007;
 		long resposta = 1;
         for (int h = 0; h < cogus.length; h++) {
             if(pulo(cogus[h], 0) == false){ 
 				resposta =( resposta * (h+1)) % mod;
 				System.out.println(resposta);
-			}
+		}
         }
-	}
+}
 	public static boolean pulo(int[] cogu, int indice){
 		int tam = cogu.length - 1;
+
+		if(cogu[tam] == 0 || cogu[0] == 0){return false;}	
 		if(indice > tam){indice = tam;}
-		int valor = cogu[indice];
 		int x = 0;
 		
-		if(cogu[tam] == 0 || cogu[0] == 0){return false;}
-		if(indice == tam && cogu[tam] > 0){return true;}
-		for(int i = cogu[indice];; i--){
-			if(indice + i > tam){if(cogu[tam] != 0){x = i;break;}}
-				else if(cogu[indice+i] != 0){x = i; break;}
-					else if(i == 0){return false;}
+		if(indice >= tam && cogu[tam] > 0){return true;}
+		for(int i = cogu[indice];i > 0; i--){
+			if(indice + i <= tam && cogu[indice+i] > 0){x = i; break;}
 		}
-		return pulo(cogu,valor+x);
+		if(x == 0){return false;}
+		return pulo(cogu,indice+x);
 	}
 }
